4d0a842263c32ef8e0cfffa1482baf3779f35c3b
Zijian Zhang <futrime@outlook.com>
2023-11-22 18:34:06 +0800

    build: add deps

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,25 @@
-add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
-add_requires("bdslibrary 1.20.41.02", "symbolprovider ^1.1.0")
+-- add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
+add_requires("levilamina ^0.1.0")
+
+package("levilamina")
+    add_deps("fmt", "magic_enum", "nlohmann_json")
+    -- add_urls("https://github.com/LiteLDev/LeviLamina/releases/download/v$(version)/levilamina-windows-x64.zip")
+    -- add_versions(v0.1.0", "bc4ee0cd9cbb59a3e57031b150ad990c17a7355474dfefac6ad3e05ef785bcd3")
+
+    add_urls("https://github.com/LiteLDev/LeviLamina.git")
+    add_versions("0.1.0", "96f2484f123d4ce808debd20de0dfd9256ffdd72")
+
+    on_install(function (package)
+        -- os.cp("include", package:installdir())
+        -- os.cp("lib/*.lib", package:installdir("lib"))
+
+        import("package.tools.xmake").install(package)
+    end)
 
 target("plugin")
     add_files("src/**.cc")
     add_includedirs("include")
+    add_packages("levilamina")
     add_rules("mode.debug", "mode.release")
     set_arch("x64")
     set_kind("shared")
