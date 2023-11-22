2a6e887933177d9991711764d4c7c6a59f19cb00
Zijian Zhang <futrime@outlook.com>
2023-11-23 07:53:25 +0800

    feat: add code

--- src/plugin.cc
+++ src/plugin.cc
@@ -1,1 +1,9 @@
+#include <ll/api/plugin/Plugin.h>
 
+extern "C" bool ll_plugin_load(ll::plugin::Plugin &self) { return true; }
+
+extern "C" bool ll_plugin_unload(ll::plugin::Plugin &self) { return true; }
+
+extern "C" bool ll_plugin_enable(ll::plugin::Plugin &self) { return true; }
+
+extern "C" bool ll_plugin_disable(ll::plugin::Plugin &self) { return true; }
--- xmake.lua
+++ xmake.lua
@@ -1,20 +1,5 @@
--- add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
+add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 add_requires("levilamina ^0.1.0")
-
-package("levilamina")
-    add_deps("fmt", "magic_enum", "nlohmann_json")
-    -- add_urls("https://github.com/LiteLDev/LeviLamina/releases/download/v$(version)/levilamina-windows-x64.zip")
-    -- add_versions(v0.1.0", "bc4ee0cd9cbb59a3e57031b150ad990c17a7355474dfefac6ad3e05ef785bcd3")
-
-    add_urls("https://github.com/LiteLDev/LeviLamina.git")
-    add_versions("0.1.0", "96f2484f123d4ce808debd20de0dfd9256ffdd72")
-
-    on_install(function (package)
-        -- os.cp("include", package:installdir())
-        -- os.cp("lib/*.lib", package:installdir("lib"))
-
-        import("package.tools.xmake").install(package)
-    end)
 
 target("plugin")
     add_files("src/**.cc")
