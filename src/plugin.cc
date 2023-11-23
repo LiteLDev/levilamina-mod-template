0178d72d1d5dfccc97df832ff7959ca3542a6b11
Futrime <futrime@outlook.com>
2023-11-23 09:52:20 +0800

    feat: modify a little

--- src/plugin.cc
+++ src/plugin.cc
@@ -13,22 +13,21 @@ ll::Logger const &Plugin::GetLogger() const { return this->logger_; }
 std::string_view Plugin::GetName() const { return this->plugin_name_; }
 
 bool Plugin::Load(ll::plugin::Plugin &self) {
-  this->logger_.info("Test");
-  this->logger_.info("{} loaded!", this->plugin_name_);
+  this->logger_.info("{} loaded.", this->plugin_name_);
   return true;
 }
 
 bool Plugin::Unload(ll::plugin::Plugin &self) {
-  this->logger_.info("{} unloaded!", this->plugin_name_);
+  this->logger_.info("{} unloaded.", this->plugin_name_);
   return true;
 }
 
 bool Plugin::Enable(ll::plugin::Plugin &self) {
-  this->logger_.info("{} enabled!", this->plugin_name_);
+  this->logger_.info("{} enabled.", this->plugin_name_);
   return true;
 }
 
 bool Plugin::Disable(ll::plugin::Plugin &self) {
-  this->logger_.info("{} disabled!", this->plugin_name_);
+  this->logger_.info("{} disabled.", this->plugin_name_);
   return true;
 }
