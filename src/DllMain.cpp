b0de6660775d4278dca2485359f13a81193e3f80
OEOTYAN <58554322+OEOTYAN@users.noreply.github.com>
2023-12-25 09:22:30 +0800

    refactor: remove useless things

--- src/DllMain.cpp
+++ src/DllMain.cpp
@@ -9,12 +9,6 @@ Plugin plugin;
 
 extern "C" {
 _declspec(dllexport) bool ll_plugin_load(ll::plugin::Plugin& self) { return plugin.load(self); }
-
-_declspec(dllexport) bool ll_plugin_unload(ll::plugin::Plugin& self) { return plugin.unload(self); }
-
-_declspec(dllexport) bool ll_plugin_enable(ll::plugin::Plugin& self) { return plugin.enable(self); }
-
-_declspec(dllexport) bool ll_plugin_disable(ll::plugin::Plugin& self) { return plugin.disable(self); }
 }
 
 } // namespace plugins
