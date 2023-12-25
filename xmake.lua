bb7555715f448524b81167a280bd94e31d03730c
Zijian Zhang <futrime@outlook.com>
2023-12-25 16:52:40 +0800

    build: use liteldev's xmake-repo

--- xmake.lua
+++ xmake.lua
@@ -1,51 +1,10 @@
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 
-add_requires("levilamina 0.2.1")
+add_requires("levilamina *")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
-
-package("levilamina")
-    add_urls("https://github.com/LiteLDev/LeviLamina/releases/download/v$(version)/levilamina-windows-x64.zip")
-    add_versions("0.2.1", "995a6979ea2e42f597c34dfa06ffe273909eab84a55943e51ffceb4ce39e82ad")
-
-    on_load(function (package)
-        import("core.base.semver")
-
-        local dependencies = {
-            ["*"] = {
-                "ctre 3.8.1",
-                "entt 3.12.2",
-                "fmt 10.1.1",
-                "gsl 4.0.0",
-                "leveldb 1.23",
-                "magic_enum 0.9.0",
-                "nlohmann_json 3.11.2",
-                "rapidjson 1.1.0",
-                "pcg_cpp 1.0.0",
-                "pfr 2.1.1",
-                "preloader 1.4.0",
-                "symbolprovider 1.1.0"
-            },
-            ["0.2.*"] = {
-                "bdslibrary 1.20.50.03"
-            }
-        }
-
-        for key, value in pairs(dependencies) do
-            if semver.satisfies(package:version_str(), key) then
-                for _, dependency in ipairs(value) do
-                    package:add("deps", dependency)
-                end
-            end
-        end
-    end)
-
-    on_install(function (package)
-        os.cp("include", package:installdir())
-        os.cp("lib/*.lib", package:installdir("lib"))
-    end)
 
 target("levilamina-plugin-template") -- Change this to your plugin name.
     add_cxflags(
