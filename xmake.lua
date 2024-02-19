c949be05235b94e675b80b757753e2da2ecd8f9d
RimuruChan <RealSprite233@outlook.com>
2024-02-20 00:43:29 +0800

    build: update build script

--- xmake.lua
+++ xmake.lua
@@ -2,6 +2,9 @@ add_rules("mode.debug", "mode.release")
 
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 
+-- add_requires("levilamina x.x.x") for a specific version
+-- add_requires("levilamina develop") to use develop version
+-- please note that you should add bdslibrary yourself if using dev version
 add_requires("levilamina")
 
 if not has_config("vs_runtime") then
@@ -9,30 +12,15 @@ if not has_config("vs_runtime") then
 end
 
 target("change-this") -- Change this to your plugin name.
-    add_cxflags(
-        "/EHa", -- To catch both structured (asynchronous) and standard C++ (synchronous) exceptions.
-        "/utf-8" -- To enable UTF-8 source code.
-    )
-    add_defines(
-        "_HAS_CXX23=1", -- To enable C++23 features
-        "NOMINMAX", -- To avoid conflicts with std::min and std::max.
-        "UNICODE" -- To enable Unicode support in Windows API.
-    )
-    add_files(
-        "src/**.cpp"
-    )
-    add_includedirs(
-        "src"
-    )
-    add_packages(
-        "levilamina"
-    )
-    add_shflags(
-        "/DELAYLOAD:bedrock_server.dll" -- To use forged symbols of SymbolProvider.
-    )
+    add_cxflags("/EHa", "/utf-8")
+    add_defines("NOMINMAX", "UNICODE")
+    add_files("src/**.cpp")
+    add_includedirs("src")
+    add_packages("levilamina")
+    add_shflags("/DELAYLOAD:bedrock_server.dll") -- To use symbols provided by SymbolProvider.
     set_exceptions("none") -- To avoid conflicts with /EHa.
     set_kind("shared")
-    set_languages("cxx20")
+    set_languages("c++20")
     set_symbols("debug")
 
     after_build(function (target)
