d1daa32a054461531d2d4d5b8427a9608ad15500
OEOTYAN <OEOTYAN@outlook.com>
2024-03-02 20:53:28 +0800

    revert: revert

--- src/change_this/Entry.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <ll/api/plugin/NativePlugin.h>
-
-namespace change_this {
-
-[[nodiscard]] auto getSelfPluginInstance() -> ll::plugin::NativePlugin&;
-
-} // namespace change_this
