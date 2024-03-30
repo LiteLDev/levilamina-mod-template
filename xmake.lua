1ecd534740cb5ae9b58c4a9096a1cdd6bf84c2e1
OEOTYAN <OEOTYAN@outlook.com>
2024-03-30 22:51:18 +0800

    refactor: add w4

--- xmake.lua
+++ xmake.lua
@@ -12,7 +12,17 @@ if not has_config("vs_runtime") then
 end
 
 target("my-plugin") -- Change this to your plugin name.
-    add_cxflags("/EHa", "/utf-8")
+    add_cxflags(
+        "/EHa",
+        "/utf-8",
+        "/W4",
+        "/w44265",
+        "/w44289",
+        "/w44296",
+        "/w45263",
+        "/w44738",
+        "/w45204"
+    )
     add_defines("NOMINMAX", "UNICODE")
     add_files("src/**.cpp")
     add_includedirs("src")
